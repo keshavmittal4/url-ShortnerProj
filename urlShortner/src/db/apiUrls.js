6c5062d432afe0130d3f9d7400a79122df1a9fdc
Keshav Mittal <keshavmittal7cr@gmail.com>
2025-01-06 22:46:59 +0530

    redirected-link through short-url

--- urlShortner/src/db/apiUrls.js
+++ urlShortner/src/db/apiUrls.js
@@ -74,26 +74,3 @@ export async function getLongUrl(id) {
 
   return data;
 }
-
-const parser = new UAParser();
-
-export const storeClicks = async ({id, original_url}) => {
-  try {
-    const res = parser.getResult();
-    const device = res.type || "desktop";
-     
-    const response = await fetch("httpps://ipapi.co/json");
-    const {city, country_name: country} = await response.json();
-
-    await supabase.from("clicks").insert({
-      url_id: id,
-      city: city,
-      country: country,
-      device: device,
-    });
-
-    window.location.href = original_url;
-  } catch (error) {
-    console.error("Error recording click: ", error)
-  }
-}
