1a0ffbc0ec6378cb3ab468148663b8323d2dc60b
Keshav Mittal <keshavmittal7cr@gmail.com>
2025-01-16 10:14:28 +0530

    deplooy changes

--- urlShortner/src/components/location-stats.jsx
+++ urlShortner/src/components/location-stats.jsx
@@ -3,11 +3,9 @@ import {
   Line,
   XAxis,
   YAxis,
-  CartesianGrid,
   Tooltip,
   Legend,
   ResponsiveContainer,
-  LabelList,
 } from "recharts";
 
 export default function Location({ stats }) {
@@ -15,17 +13,18 @@ export default function Location({ stats }) {
     const cityCount = stats.reduce((acc, item) => {
         if(acc[item.city]){
             acc[item.city] += 1;
-        } else {
-            acc[item.city] = 1;
-        }
+        } else acc[item.city] = 1;
+        
 
         return acc;
     }, {});
 
+    // console.log(cityCount);
+
     const cities = Object.entries(cityCount).map(([city, count]) =>({
         city,
         count,
-    }))
+    }));
     
   return (
     <div width={"100%"} height={300}>
@@ -35,8 +34,7 @@ export default function Location({ stats }) {
           <YAxis />
           <Tooltip labelStyle={{ color: "green" }} />
           <Legend />
-          <Line type="monotone" dataKey="count" stroke="#8884d8">
-          </Line>
+          <Line type="monotone" dataKey="count" stroke="#8884d8" />
         </LineChart>
       </ResponsiveContainer>
     </div>
